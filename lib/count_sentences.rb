a3381b8b32a7013908c226e1c8f025df27c6598e
Anslie Brant <brantae@gmail.com>
2023-02-22 20:28:52 -0600

    commit tree 5d90e43f05486a850483b5b2e99c079187d0d250

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
 require 'pry'
 
 class String
+  attr_accessor :words
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    words = self.split(/[.?!]/).reject { |c| c.empty? }
+    words.count
   end
 end
